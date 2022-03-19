e0b96bd7050d5cf4fafbc7001d2a4e46e271dcd4
RDonii <90790952+RDonii@users.noreply.github.com>
2022-03-20 04:06:09 +0500

    settings updated

--- CCS/settings.py
+++ CCS/settings.py
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 django_heroku.settings(locals())
 
